68ab4544537316eef698190980830bfc08d0678a
BowieGian <43588714+BowieGian@users.noreply.github.com>
2023-09-10 00:03:29 -0700

    Add rest of character stats
    Add getters/setters with simple range check for each stat

--- app/entities/characters/Character.tsx
+++ app/entities/characters/Character.tsx
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import Entity from "../Entity";
-import Linear, { ILinear } from "../Linear";
+import { ILinear } from "../Linear";
 import BaseStats, { IBaseStatData } from "../BaseStats";
 
 export interface ICharacterData {
@@ -12,7 +12,16 @@ export interface ICharacterData {
 }
 
 export default class Character extends Entity {
-  private _critRate: number = 5;
+  private _critRate: number = .05;
+  private _critDamage: number = .5;
+  private _breakEffect: number = 0;
+  private _healingBoost: number = 0;
+  private _maxEnergy: number = NaN; // TODO: Consider adding to constructor
+  private _energyRegen: number = 1;
+  private _effectHit: number = 0;
+  private _effectRes: number = 0;
+  private _elementalDmg: number = 0; // TODO: Consider single vs mult element
+  private _elementalRes: number = 0;
 
   constructor(name: string, level: number) {
     let baseStats: BaseStats;
@@ -24,21 +33,112 @@ export default class Character extends Entity {
 
       baseStats = new BaseStats(characterData.baseStats);
       console.log(baseStats);
-    } catch (err) {
+    } catch (err) { // Catch JSON.parse error
       console.error(err);
       return;
     }
-    
+
     super(name, level, baseStats);
   }
 
+
+  // Getters & Setters
   public get critRate(): number {
     return this._critRate;
   }
 
   public set critRate(value: number) {
     if (value < 0) throw new RangeError("Crit Rate cannot be negative");
-    
+
     this._critRate = value;
+  }
+
+  public get critDamage(): number {
+    return this._critDamage;
+  }
+
+  public set critDamage(value: number) {
+    if (value < 0) throw new RangeError("Crit Dmg cannot be negative");
+
+    this._critDamage = value;
+  }
+
+  public get breakEffect(): number {
+    return this._breakEffect;
+  }
+
+  public set breakEffect(value: number) {
+    if (value < 0) throw new RangeError("Break Effect cannot be negative");
+
+    this._breakEffect = value;
+  }
+
+  public get healingBoost(): number {
+    return this._healingBoost;
+  }
+
+  public set healingBoost(value: number) {
+    if (value < 0) throw new RangeError("Healing Boost cannot be negative");
+
+    this._healingBoost = value;
+  }
+
+  public get maxEnergy(): number {
+    return this._maxEnergy;
+  }
+
+  public set maxEnergy(value: number) {
+    if (value < 0) throw new RangeError("Max Energy cannot be negative");
+
+    this._maxEnergy = value;
+  }
+
+  public get energyRegen(): number {
+    return this._energyRegen;
+  }
+
+  public set energyRegen(value: number) {
+    if (value < 0) throw new RangeError("Energy Regen cannot be negative");
+
+    this._energyRegen = value;
+  }
+
+  public get effectHit(): number {
+    return this._effectHit;
+  }
+
+  public set effectHit(value: number) {
+    if (value < 0) throw new RangeError("Effect Hit cannot be negative");
+
+    this._effectHit = value;
+  }
+
+  public get effectRes(): number {
+    return this._effectRes;
+  }
+
+  public set effectRes(value: number) {
+    if (value < 0) throw new RangeError("Effect Res cannot be negative");
+    this._effectRes = value;
+  }
+
+  public get elementalDmg(): number {
+    return this._elementalDmg;
+  }
+
+  public set elementalDmg(value: number) {
+    if (value < 0) throw new RangeError("Elemental Dmg cannot be negative");
+
+    this._elementalDmg = value;
+  }
+
+  public get elementalRes(): number {
+    return this._elementalRes;
+  }
+
+  public set elementalRes(value: number) {
+    if (value < 0) throw new RangeError("Elemental Res cannot be negative");
+
+    this._elementalRes = value;
   }
 }
