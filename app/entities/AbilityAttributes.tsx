78a44359a57188d8e1455e5e74252a70cd49cad9
BowieGian <43588714+BowieGian@users.noreply.github.com>
2023-10-05 01:30:34 -0700

    Add AbilityAttributes calculate funcs
    * Store calculated attributes in class
        - Add getters

--- app/entities/AbilityAttributes.tsx
+++ app/entities/AbilityAttributes.tsx
@@ -24,6 +24,13 @@ export default class AbilityAttributes {
     talent: new Array<Attribute>
   }
 
+  private attributes = {
+    basic: new Array<number>,
+    skill: new Array<number>,
+    ult: new Array<number>,
+    talent: new Array<number>
+  }
+
   constructor(input: IAbilityAttributeData) {
     let property: keyof IAbilityAttributeData;
     for (property in input) {
@@ -33,7 +40,28 @@ export default class AbilityAttributes {
     }
   }
 
-  private printAttribute(ability: AbilityTypes) {
+  public calculateAttribute(level: number, ability: AbilityTypes): void {
+    for (let i = 0; i < this.data[ability].length; i++) {
+      this.attributes[ability][i] = this.data[ability][i].calculate(level);
+    }
+  }
+
+  public calculateAll(
+    abilityLevels: {
+      basic: number,
+      skill: number,
+      ult: number,
+      talent: number
+    }
+  ): void {
+
+    let abilities: AbilityTypes[] = ["basic" , "skill" , "ult" , "talent"];
+
+    abilities.forEach(ability =>
+      this.calculateAttribute(abilityLevels[ability], ability));
+  }
+
+  private printAttribute(ability: AbilityTypes): void {
     console.log(ability);
     for (let i = 0; i < this.data[ability].length; i++) {
       console.log("Attribute %d", i);
@@ -55,5 +83,29 @@ export default class AbilityAttributes {
     let abilities: AbilityTypes[] = ["basic" , "skill" , "ult" , "talent"];
 
     abilities.forEach(ability => this.printAttribute(ability));
+  }
+
+  /*--------------------------------------------------------------*/
+  /* Getters & Setters                                            */
+  /*--------------------------------------------------------------*/
+
+  public get basic(): ReadonlyArray<number> {
+    let output: ReadonlyArray<number> = this.attributes.basic;
+    return output;
+  }
+
+  public get skill(): ReadonlyArray<number> {
+    let output: ReadonlyArray<number> = this.attributes.skill;
+    return output;
+  }
+
+  public get ult(): ReadonlyArray<number> {
+    let output: ReadonlyArray<number> = this.attributes.ult;
+    return output;
+  }
+
+  public get talent(): ReadonlyArray<number> {
+    let output: ReadonlyArray<number> = this.attributes.talent;
+    return output;
   }
 }
