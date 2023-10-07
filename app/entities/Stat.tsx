0d1bdf397b02c15b75195fa1cb495f60dc75d5e4
BowieGian <43588714+BowieGian@users.noreply.github.com>
2023-10-06 18:49:18 -0700

    Remove unnecessary setters

--- app/entities/Stat.tsx
+++ app/entities/Stat.tsx
@@ -4,30 +4,19 @@ export interface IStat {
 }
 
 export default class Stat {
-  private _base: number = 0;
-  private _add: number = 0;
+  private base: number = 0;
+  private add: number = 0;
 
   constructor(input: IStat) {
+    if (input.base < 0) throw new RangeError("Base cannot be negative");
+    if (input.add < 0) throw new RangeError("Add cannot be negative");
+
     this.base = input.base;
     this.add = input.add;
   }
 
   public calculate(level: number, ascension: number = 0): number {
-    let output = this._base + this._add * (level - 1 + 8 * ascension)
+    let output = this.base + this.add * (level - 1 + 8 * ascension)
     return Math.round((output + Number.EPSILON) * 1e6) / 1e6;
-  }
-
-  /*--------------------------------------------------------------*/
-  /* Getters & Setters                                            */
-  /*--------------------------------------------------------------*/
-
-  private set base(value: number) {
-    if (value < 0) throw new RangeError("Base cannot be negative");
-    this._base = value;
-  }
-
-  private set add(value: number) {
-    if (value < 0) throw new RangeError("Add cannot be negative");
-    this._add = value;
   }
 }
