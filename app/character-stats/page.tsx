8a95e2aed0304b0a3d21daf55e31da97ec75cfc3
BowieGian <43588714+BowieGian@users.noreply.github.com>
2023-10-07 18:56:46 -0700

    Update title

--- app/character-stats/page.tsx
+++ app/character-stats/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-5 bg-neutral-800 lg:p-24">
       <h1 className="my-14 lg:mt-0 text-center text-3xl font-bold uppercase tracking-widest text-purple-200">
-        Star Rail Character Stats
+        Yanqing Character Stats
       </h1>
 
       <Link href={"/"} className="font-sans font-bold text-purple-200">
