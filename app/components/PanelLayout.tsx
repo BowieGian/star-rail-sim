73c50df81341e400bbaaee473d273c9230563273
BowieGian <43588714+BowieGian@users.noreply.github.com>
2023-10-24 18:39:59 -0700

    Increase max width

--- app/components/PanelLayout.tsx
+++ app/components/PanelLayout.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function PanelLayout({ children }: Props) {
   return (
-    <div className="w-full max-w-3xl overflow-hidden rounded-xl bg-neutral-700 shadow-lg">
+    <div className="w-full max-w-6xl overflow-hidden rounded-xl bg-neutral-700 shadow-lg">
       <div className="px-4 py-5 sm:p-6">{children}</div>
     </div>
   );
