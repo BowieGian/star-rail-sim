da584e423305b44212644443d441a9f1d7cde4f9
BowieGian <43588714+BowieGian@users.noreply.github.com>
2023-10-28 03:16:21 -0700

    Add isAscended function

--- app/entities/characters/Character.tsx
+++ app/entities/characters/Character.tsx
@@ -114,6 +114,10 @@ export default class Character extends Entity {
     return false;
   }
 
+  public isAscended(): boolean {
+    return this.ascensionFromLevel(this.level) + 1 === this.ascension;
+  }
+
   /*--------------------------------------------------------------*/
   /* Private Functions                                            */
   /*--------------------------------------------------------------*/
