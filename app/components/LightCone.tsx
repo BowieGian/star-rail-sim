3c522113c73f9bb8f8a6c005506eccbf045104a5
Chanson Tang <90084138+chanchantang@users.noreply.github.com>
2023-12-20 22:37:40 -0800

    Add temp lightcone output

--- app/components/LightCone.tsx
+++ app/components/LightCone.tsx
@@ -6,6 +6,10 @@ import LvlAscInput from "./LvlAscInput";
 import { useEffect, useState } from "react";
 import { CharacterKey, characterList, Characters } from "../entities/characters";
 
+import DamageOutput from "./DamageOutput";
+
+import { IStatDisplay } from "../entities/characters/Character";
+
 export default function LightCone() {
   const [character, setCharacter] = useState<Characters>(() => characterList["Yanqing"]);
 
@@ -16,23 +20,33 @@ export default function LightCone() {
   const [ascension, setAscension] = useState<number>(0);
   const [ascended, setAscended] = useState<boolean>(false);
 
+  const [stats, setStats] = useState<readonly IStatDisplay[]>(character.getBaseStatsDisplay());
+
   const handleAscToggle = (e: React.FormEvent) => {
     let value = !ascended;
     setAscended(value);
   }
 
   return (
-    <div className="flex flex-col gap-y-8 lg:px-5 lg:py-6">
-      <LvlAscInput
-        name="char-lvl"
-        min={1}
-        max={80}
-        lvl={charLvl}
-        setLvl={setCharLvl}
-        handleButton={handleAscToggle}
-        disableButton={!ascendable}
-        maxLvlForAsc={maxLvl}
-      />
-    </div>
+    <>
+      <div className="flex flex-col gap-y-8 lg:px-5 lg:py-6">
+        <LvlAscInput
+          name="char-lvl"
+          min={1}
+          max={80}
+          lvl={charLvl}
+          setLvl={setCharLvl}
+          handleButton={handleAscToggle}
+          disableButton={!ascendable}
+          maxLvlForAsc={maxLvl}
+        />
+      </div>
+
+      <div className="flex flex-col gap-y-1 lg:px-5 lg:py-6">
+        {stats.map(function(object) {
+          return <DamageOutput key={object.key} num={object.value.toString()} label={object.name}/>;
+        })}
+      </div>
+    </>
   )
 }
