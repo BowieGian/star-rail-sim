ba190c5b9e66ed498b666244b56af5aaea654587
BowieGian <43588714+BowieGian@users.noreply.github.com>
2024-01-10 15:08:16 -0800

    Refactor `AbilityIO` to not depend on Character

--- components/AbilityIO.tsx
+++ components/AbilityIO.tsx
@@ -1,45 +1,37 @@
 import { useEffect, useState } from "react";
 import AbilityDescription from "./AbilityDescription";
 import NumberSlider from "./NumberSlider";
-import { Characters } from "@/src/entities/characters/data";
-import { CharacterAbilityTypes } from "@/src/ability/Ability";
 
 interface Props {
-  character: Characters;
-  ability: CharacterAbilityTypes;
+  label: string;
+  max: number;
+  getLevel: () => number;
+  setLevel: (value: number) => void;
+  getAttributes: () => readonly number[];
+  description: readonly string[];
 }
 
 export default function AbilityIO(props: Props) {
-  const {character, ability} = props;
-
-  const [maxLevel, setMaxLevel] = useState<number>(1);
+  const {label, max, getLevel, setLevel, getAttributes, description} = props;
 
   const [abilityLevel, setAbilityLevel] = useState<string>("1");
-  const [attributes, setAttributes] = useState<readonly number[]>(character.getAbilityAttr(ability));
-  const description = character.getAbilityDesc(ability);
+  const [attributes, setAttributes] = useState<readonly number[]>(getAttributes());
 
   useEffect(() => {
-    if (ability === "basic")
-      setMaxLevel(7);
-    else
-      setMaxLevel(12);
-  }, [ability]);
+    setAbilityLevel(getLevel().toString());
+  }, [getLevel]);
 
   useEffect(() => {
-    setAbilityLevel(character.getAbilityLevel(ability).toString());
-  }, [character, ability]);
-
-  useEffect(() => {
-    character.setAbilityLevel(ability, parseInt(abilityLevel));
-    setAttributes([...character.getAbilityAttr(ability)]);
-  }, [abilityLevel, character, ability]);
+    setLevel(parseInt(abilityLevel));
+    setAttributes([...getAttributes()]);
+  }, [abilityLevel, setLevel, getAttributes]);
 
   return (
     <>
       <div className="flex flex-col gap-y-8 lg:px-5">
-        <NumberSlider stat={abilityLevel} setStat={setAbilityLevel} name={ability + "-lvl"} label={ability} min={1} max={maxLevel}/>
+        <NumberSlider stat={abilityLevel} setStat={setAbilityLevel} name={label + "-lvl"} label={label} min={1} max={max}/>
       </div>
-      <AbilityDescription attributes={attributes} description={description} label={ability}/>
+      <AbilityDescription attributes={attributes} description={description} label={label}/>
     </>
   );
 }
--- components/CharacterAbilities.tsx
+++ components/CharacterAbilities.tsx
@@ -1,17 +1,32 @@
 import AbilityIO from "./AbilityIO";
-import { Characters } from "@/src/entities/characters/data";
+import Character from "@/src/entities/characters/Character";
 import { characterAbilityTypes } from "@/src/ability/Ability";
 
 interface Props {
-  character: Characters;
+  character: Character;
 }
 
 export default function CharacterAbilities(props: Props) {
-  const character = props.character;
+  const {character} = props;
 
   return (
     characterAbilityTypes.map((ability) => {
-      return <AbilityIO key={ability} character={character} ability={ability}/>;
+      let max: number;
+
+      if (ability === "basic")
+        max = 7;
+      else
+        max = 12;
+
+      return <AbilityIO
+        key={ability}
+        label={ability}
+        max={max}
+        getLevel={() => {return character.getAbilityLevel(ability);}}
+        setLevel={(value: number) => character.setAbilityLevel(ability, value)}
+        getAttributes={() => character.getAbilityAttr(ability)}
+        description={character.getAbilityDesc(ability)}
+      />;
     })
   );
 }
